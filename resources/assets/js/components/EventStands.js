4274aae0f7b5ce8840180d9d621b68f81962692a
NH Evan <nhevan@gmail.com>
2017-07-03 16:11:35 +0600

    Clicking on a stand now displays a modal using react-bootstrap.

--- resources/assets/js/components/EventStands.js
+++ resources/assets/js/components/EventStands.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import Stand from './Stand';
+import StandDetail from './StandDetail';
+import { Modal, Button } from 'react-bootstrap';
 
 export default class EventStands extends React.Component {
 	constructor(props) {
 		super(props);
-	}
-
-	test(e){
-		e.preventDefault();
-		console.log('clicked on stand');
-		alert(e.target.value);
+		this.state = {
+			showModal: false,
+			selectedStand : []
+		}
 	}
 
 	renderStands(stands){
 		if (stands.length > 0) {
 			return stands.map((stand) => (
-	            <rect onClick={this.test} 
+	            <rect onClick={(e) => this.loadStandDetailModal(e)} 
 	            	  className="stands" 
 	            	  key={stand.id} 
 	            	  width={stand.breadth + 'px'} 
 	            	  height={stand.length + 'px'} 
 	            	  x={stand.x_cord + 'px'} 
-	            	  y={stand.y_cord + 'px'}>
+	            	  y={stand.y_cord + 'px'}
+	            	  data-selected-stand={stand}>
         	    </rect>
 	        ));
 	    }
@@ -34,8 +35,8 @@ export default class EventStands extends React.Component {
 				if (stand.company) {
 					return <image href={stand.company.logo} 
 								  key={index} 
-								  x={ (stand.breadth/4) + stand.x_cord} 
-								  y={ (stand.length/3) - stand.y_cord}
+								  x={ (stand.breadth/2) - (stand.breadth / 4) + stand.x_cord} 
+								  y={ (stand.length/2) - (stand.length / 4) + stand.y_cord}
 								  width={stand.breadth / 2}
 								  height={stand.length / 2}
 								  fill="black" />
@@ -70,16 +71,48 @@ export default class EventStands extends React.Component {
 		else return [];
 	}
 
+	closeModal(){
+		this.setState({ showModal: false });
+	}
+
+	openModal(){
+		console.log('closing modal ...');
+	    this.setState({ showModal: true });
+	}
+
+	loadStandDetailModal(e){
+		console.log('clicked on a stand');
+		e.preventDefault();
+		var selected_stand = e.target.getAttribute('data-selected-stand');
+		this.setState({
+			selectedStand: selected_stand
+		});
+		this.openModal();
+	}
+
 	render() {
 		const stands = this.renderStands(this.props.stands);
 		const infos = this.renderInfos(this.props.stands);
 		const stand_statuses = this.renderStatuses(this.props.stands);
 		return (
+			<div>
 				<svg width="700px" height="400px">
 					{ stands }
 					{ infos }
 					{ stand_statuses }
 				</svg>
+				<Modal show={this.state.showModal} onHide={() => this.closeModal()}>
+					<Modal.Header closeButton>
+			            <Modal.Title>Stand Detail</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+			            <StandDetail stand={this.state.selectedStand} />
+		            </Modal.Body>
+		            <Modal.Footer>
+						<Button onClick={() => this.closeModal()}>Close</Button>
+					</Modal.Footer>
+				</Modal>
+			</div>
 		);
 	}
 }
